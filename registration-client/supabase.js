f3fdb4b7dfe0f6198a8066b0543de70679954aa5
Rohit Anish <rohitanish158@gmail.com>
2025-02-05 15:15:05 +0530

    changed supabase.js

--- registration-client/supabase.js
+++ registration-client/supabase.js
@@ -6,4 +6,11 @@ console.log('SUPABASE KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+    auth: {
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: true,
+      redirectTo: "https://mec-cupid.onrender.com/auth/callback"
+    }
+  });

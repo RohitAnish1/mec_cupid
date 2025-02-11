c57b0744ae69e982f74ad6697188e40eb0fa1e91
aaron-6960 <aaroongeorge2005@gmail.com>
2025-02-11 23:27:52 +0530

    added ui changes

--- registration-client/src/components/Login.jsx
+++ registration-client/src/components/Login.jsx
@@ -33,11 +33,11 @@ export default function Login({ onLogin }) {
   return (
     <div className="relative flex flex-col items-center justify-center h-screen overflow-hidden">
       <div className="absolute -top-10 flex flex-col gap-2 h-20">
-        <img src={Logo} alt="Logo"/>
+        <img src={Logo} alt="Logo" />
         <h4 className="text-xl absolute text-center w-full top-36 text-white font-medium">Fortitude MEC</h4>
       </div>
-      <h1 className="text-[32px] mb-2 pt-4">Welcome to MEC Cupid</h1>
-      <img src={LoginImage} alt="Login Image" className="rounded-xl mb-6 w-[90vw]" />
+      <h1 className="text-[32px] mb-2 pt-4 mt-10">Welcome to MEC Cupid</h1>
+      <img src={LoginImage} alt="Login Image" className="rounded-xl mb-6 w-[80%] xs:w-[60%] sm:w-[50%]" />
       <button
         onClick={handleGoogleLogin}
         className="bg-white rounded-[100px] px-4 py-2 text-[40px] flex items-center gap-2 max-h-[68px]"
